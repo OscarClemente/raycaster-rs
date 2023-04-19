6b141fb369baf59e39727d25f7afe4b12c560a05
OscarClemente <oscar.clemente@gmail.com>
2023-04-19 17:46:52 +0200

    feat: WIP adding renderer and gamecontext

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+extern crate sdl2;
+
 use std::ops::Add;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::WindowCanvas;
+use sdl2::video::Window;
 
 const MAP_WIDTH: usize = 24;
 const MAP_HEIGHT: usize = 24;
@@ -80,6 +88,7 @@ const WORLD_MAP: [&[i32; MAP_WIDTH]; MAP_HEIGHT] = [
     ],
 ];
 
+#[derive(Clone, Copy, PartialEq)]
 pub struct Vec2D {
     x: f64,
     y: f64,
@@ -102,11 +111,85 @@ impl Vec2D {
     }
 }
 
-fn main() {
+
+pub struct Renderer {
+    canvas: WindowCanvas,
+}
+
+impl Renderer {
+    pub fn new(window: Window) -> Result<Renderer, String> {
+        let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+        Ok(Renderer { canvas })
+    }
+
+//    fn draw_dot(&mut self, point: &Point) -> Result<(), String> {
+//        let Point(x, y) = point;
+//        self.canvas.fill_rect(Rect::new(
+//            x * DOT_SIZE_IN_PXS as i32,
+//            y * DOT_SIZE_IN_PXS as i32,
+//            DOT_SIZE_IN_PXS.try_into().unwrap(),
+//            DOT_SIZE_IN_PXS.try_into().unwrap(),
+//        ))?;
+//
+//        Ok(())
+//    }
+
+    pub fn draw(&mut self, context: &GameContext) -> Result<(), String> {
+        self.draw_background(context);
+        self.canvas.present();
+
+        Ok(())
+    }
+
+    fn draw_background(&mut self, context: &GameContext) {
+        let color = Color::RED;
+        self.canvas.set_draw_color(color);
+        self.canvas.clear();
+    }
+}
+
+pub struct GameContext {
+}
+
+impl GameContext {
+    pub fn new() -> GameContext {
+        GameContext {
+        }
+    }
+    pub fn next_tick(&mut self) {
+    }
+}
+
+fn main() -> Result<(), String> {
     let mut player_position = Vec2D::new(22.0, 12.0);
     let mut player_direction = Vec2D::new(-1.0, 0.0);
     let mut plane_position = Vec2D::new(0.0, 0.66);
 
     let mut time = 0.0; //time of current frame
-    let mut oldTime = 0.0; //time of previous frame
+    let mut old_time = 0.0; //time of previous frame
+
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window(
+            "Snake Game",
+            (SCREEN_WIDTH).try_into().unwrap(),
+            (SCREEN_HEIGHT).try_into().unwrap(),
+        )
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    let mut context = GameContext::new();
+    let mut renderer = Renderer::new(window)?;
+    renderer.draw(&context)?;
+    let mut frame_counter = 0;
+
+
+    Ok(())
 }
+
